1a6402ade3359549e4ba506fab3525751ce360b1
harshil <harshil.mevasa@gmail.com>
2021-06-28 19:42:42 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+const title = document.querySelector(".title");
+const emoji = document.querySelector(".emoji img");
+
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(150px)";
+  emoji.style.transform = "translateZ(60px) ";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  emoji.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+});
